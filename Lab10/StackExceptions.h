1091451e43eb7caef002ec061409409c0045abae
Shashwat <mishras8@mail.uc.edu>
2023-11-23 00:03:24 -0500

    Lab 10 and HW 4

---
+++ Lab10/StackExceptions.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <exception>
+#include <string>
+
+using namespace std;
+
+class StackUnderflowException : public std::exception // When the stack is empty, throw this exception
+{
+public:
+    string what() // overrides what() virtual function from exception that returns an explanatory string
+    {
+        return "The stack is empty.";
+    }
+};
+
+class StackOverflowException : public std::exception // When the stack is full, throw this exception
+{
+public:
+    string what() // overrides what() virtual function from exception that returns an explanatory string
+    {
+        return "The stack is full.";
+    }
+};
